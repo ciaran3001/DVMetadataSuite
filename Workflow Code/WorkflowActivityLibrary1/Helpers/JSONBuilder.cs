7889bf3648417143706ec4033eeede0afb862557
Ciaran Kelly <CKelly@storm.ie>
2023-11-09 20:37:45 +0000

    Started JSON helper class to work with any object

--- Workflow Code/WorkflowActivityLibrary1/Helpers/JSONBuilder.cs
+++ Workflow Code/WorkflowActivityLibrary1/Helpers/JSONBuilder.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DVMetadataCWAs.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,54 @@ namespace DVMetadataCWAs.Helpers
      {
 
 
+          public string ConvertListToJSONArray<T>(List<T> ListData)
+          {
+               List<T> columnsInfo = null; //RetrieveColumns(service, _logicalName);
+
+               // Build JSON manually
+               StringBuilder jsonBuilder = new StringBuilder();
+               jsonBuilder.Append("[");
+               bool isFirstItem = true;
+
+               Type objType = ListData.First().GetType();
+               
+               // Get all properties of the object using reflection
+               PropertyInfo[] properties = objType.GetProperties();
+               foreach(var Item in ListData) {
+                    // Print the names and values of properties
+                    foreach (var property in properties)
+                    {
+                         object propertyValue = property.GetValue(Item);
+                         Console.WriteLine($"Property Name: {property.Name}, Property Value: {propertyValue}");
+                    }
+               }
+
+
+
+               foreach (var column in columnsInfo)
+               {    
+                    
+                    if (!isFirstItem)
+                    {
+                         jsonBuilder.Append(",");
+                    }
+                    jsonBuilder.Append($"{{\"ColumnName\":\"{EscapeJsonString( (column.ColumnName)}\"," +
+                                       $"\"DataType\":\"{EscapeJsonString(column.DataType)}\"," +
+                                       $"\"ReferencedTable\":\"{EscapeJsonString(column.ReferencedTable)}\"}}");
+                    isFirstItem = false;
+               }
+               jsonBuilder.Append("]");
+
+               return jsonBuilder.ToString();
+          }
+          private string EscapeJsonString(string input)
+          {
+               if (input == null)
+               {
+                    return null;
+               }
+               return input.Replace("\\", "\\\\").Replace("\"", "\\\"");
+          }
 
      }
 }
